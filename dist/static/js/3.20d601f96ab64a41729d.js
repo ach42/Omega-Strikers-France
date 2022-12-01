0f052caa7b24edc9aa5cacbdc584b74de7773b68
ach42 <87209387+ach42@users.noreply.github.com>
2022-12-01 14:19:04 +0100

    add build

---
+++ dist/static/js/3.20d601f96ab64a41729d.js
@@ -0,0 +1,2 @@
+webpackJsonp([3],{c5Mg:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0,!1,!1)},l=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("Page not Found")])])}],u={render:r,staticRenderFns:l},c=u,s=t("VU/8"),a=s(null,c,!1,null,null,null);n.default=a.exports}});
+//# sourceMappingURL=3.20d601f96ab64a41729d.js.map
