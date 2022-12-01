40abf248cd1e75a1ec224621715f3f1842a08d27
ach42 <87209387+ach42@users.noreply.github.com>
2022-12-01 14:27:58 +0100

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,3 @@
-build: {
-    // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
-
-    // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: './', 
-}
+module.exports = {
+    publicPath: ‘./’
+    }
